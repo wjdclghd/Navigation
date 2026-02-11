65b2afd33f66a1413c072c0b89230957144705c3
jch <ba2328@gmail.com>
2026-02-11 21:21:05 +0900

    [navigation-fundation] Package.swift 파일 추가

---
+++ Package.swift
@@ -0,0 +1,37 @@
+// swift-tools-version: 5.9
+
+//
+//  Project.swift
+//  Navigation
+//
+//  Created by jch on 2/11/26.
+//
+
+import PackageDescription
+
+let package = Package(
+    name: "Navigation",
+    
+    platforms: [
+        .iOS(.v15)
+    ],
+    products: [
+        .library(
+            name: "Navigation",
+            targets: ["Navigation"]
+        )
+    ],
+    
+    targets: [
+        .target(
+            name: "Navigation",
+            dependencies: [],
+            path: "Sources/Navigation"
+        ),
+        .testTarget(
+            name: "NavigationTests",
+            dependencies: ["Navigation"],
+            path: "Tests/NavigationTests"
+        )
+    ]
+)
