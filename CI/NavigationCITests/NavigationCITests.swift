d951e3dbb0fa1434c3a716afebdeb5821acf1fbe
jch <ba2328@gmail.com>
2026-03-22 11:36:22 +0900

    [develop] Navigation Module SPM 및 CI wrapper project 기본 셋팅

---
+++ CI/NavigationCITests/NavigationCITests.swift
@@ -0,0 +1,15 @@
+//
+//  NavigationCITests.swift
+//  NavigationCITests
+//
+//  Created by jch on 3/21/26.
+//
+
+import XCTest
+@testable import Navigation
+
+final class NavigationCITests: XCTestCase {
+    func test_importNavigationModule() {
+        XCTAssertTrue(true)
+    }
+}
--- fastlane/README.md
+++ fastlane/README.md
@@ -21,7 +21,7 @@ For _fastlane_ installation instructions, see [Installing _fastlane_](https://do
 [bundle exec] fastlane ios unit_test
 ```
 
-unit test
+Navigation unit test
 
 ----
 
